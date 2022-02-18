36f5ff27ad55bbbd1a4496bf30b7db7e37768285
andres74ar@gmail.com <andres74ar@gmail.com>
2022-02-18 12:08:24 -0300

    rutas 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+import { PrincipalComponent } from './principal/principal.component';
+import { TrbajosYestudiosComponent } from './trbajos-yestudios/trbajos-yestudios.component';
+const routes: Routes = [
+  { path: 'principal', component: PrincipalComponent },
+  { path: 'trabajos', component: TrbajosYestudiosComponent },
+  { path: 'acerca', component: AcercaDeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

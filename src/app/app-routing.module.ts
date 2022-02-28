98c50cabe57f6060a9817a32a1d7e831ce0f1616
andres74ar@gmail.com <andres74ar@gmail.com>
2022-02-28 10:10:40 -0300

    vista del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
+import { LoginComponent } from './login/login.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { RegisterComponent } from './register/register.component';
 import { TrabajosComponent } from './trabajos/trabajos.component';
 const routes: Routes = [
-  { path: 'principal', component: PrincipalComponent },
-  { path: 'trabajos', component: TrabajosComponent },
-  { path: 'acerca', component: AcercaDeComponent }
+  { path: "", component: AppComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "register", component: RegisterComponent, pathMatch: "full" },
+  { path: 'principal', component: PrincipalComponent, pathMatch: "full" },
+  { path: 'trabajos', component: TrabajosComponent, pathMatch: "full" },
+  { path: 'acerca', component: AcercaDeComponent, pathMatch: "full" }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +11,8 @@ import { PrincipalComponent } from './principal/principal.component';
 import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 import { TrabajosComponent } from './trabajos/trabajos.component';
 import { EstudiosComponent } from './estudios/estudios.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +21,14 @@ import { EstudiosComponent } from './estudios/estudios.component';
     PrincipalComponent,
     AcercaDeComponent,
     TrabajosComponent,
-    EstudiosComponent
+    EstudiosComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],

620b8c28a840b55a1ea9cf8f428ac7f7ff5755f9
andres74ar@gmail.com <andres74ar@gmail.com>
2022-05-06 15:30:13 -0300

    terminado falta pulir pero camina

---
+++ src/app/Trabajo.ts
@@ -0,0 +1,10 @@
+export interface Trabajo {
+    id?:number;
+    empresa:string;
+    puesto:string;
+    logo:string;
+    lugar:string;
+    duracion:number;
+    ingreso:number;
+    egreso:number;
+}
--- src/app/datos-porfolio.service.ts
+++ src/app/datos-porfolio.service.ts
@@ -5,6 +5,7 @@ import { AcercaDe } from './AcercaDe';
 import { Estudios } from './estudios';
 import { PrincipalImg } from './pricipalImg';
 import { PrincipalDato } from './principalDato';
+import { Trabajo } from './Trabajo';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json'
@@ -14,54 +15,71 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class DatosPorfolioService {
-  private apiUrl = 'http://localhost:5000/educacion'
+  private apiUrlEst = 'http://localhost:5000/educacion'
+  private apiUrlTrab = 'http://localhost:5000/trabajo'
   constructor(private http: HttpClient) { }
-  // Todos los obtener <<--GET-->>
+
+
+  //      <<Todo de acerca de>> 
+  obtenerDatosAcercaDe(): Observable<any> {
+    return this.http.get('http://localhost:5000/acercaDe');
+  }
+  formAcercaDe(acercaDe: AcercaDe): Observable<AcercaDe> {
+    return this.http.post<AcercaDe>('http://localhost:5000/acercaDe', acercaDe, httpOptions);
+  }
+  //      <<Fin de acerca de>>
+
+  //      <<Todo de Principal>>
   obtenerDatosPrincipal(): Observable<any> {
     return this.http.get('http://localhost:5000/principalDato');
   }
   obtenerImgPrincipal(): Observable<any> {
     return this.http.get('http://localhost:5000/principalImg');
   }
-
-  obtenerDatosEducacion(): Observable<any> {
-    return this.http.get('http://localhost:5000/educacion');
-  }
-  obtenerDatosTrabajo(): Observable<any> {
-    return this.http.get('http://localhost:5000/trabajo');
-  }
-  obtenerDatosAcercaDe(): Observable<any> {
-    return this.http.get('http://localhost:5000/acercaDe');
-  }
-
-
-
-
-
-  // Todos los edid y agregar <<--POST-->>
-  formAcercaDe(acercaDe: AcercaDe): Observable<AcercaDe> {
-    return this.http.post<AcercaDe>('http://localhost:5000/acercaDe', acercaDe, httpOptions);
-  }
   formPrincipalDato(principalDato: PrincipalDato): Observable<PrincipalDato> {
     return this.http.post<PrincipalDato>('http://localhost:5000/principalDato', principalDato, httpOptions);
   }
+
   formPrincipalImg(principalImg: PrincipalImg): Observable<PrincipalImg> {
     return this.http.post<PrincipalImg>('http://localhost:5000/principalImg', principalImg, httpOptions);
   }
-  formEstudios(estudios: Estudios): Observable<Estudios> {
-    return this.http.post<Estudios>('http://localhost:5000/educacion', estudios, httpOptions);
+  //      <<Fin de Principal>>  
+
+  //      <<Todo de Educacion>> 
+  obtenerDatosEducacion(): Observable<any> {
+    return this.http.get(this.apiUrlEst);
   }
-  //borrarEstudio(estudios:Estudios): Observable<Estudios> {
-  //  return this.http.delete<Estudios>( `${this.'http://localhost:5000/educacion'}/${estudios.id} ;
-  //}
+  formEstudios(estudios: Estudios): Observable<Estudios> {
+    return this.http.post<Estudios>(this.apiUrlEst, estudios, httpOptions);
+  }
   deleteEstudio(estudio: Estudios): Observable<Estudios> {
-    const url = `${this.apiUrl}/${estudio.id}`
+    const url = `${this.apiUrlEst}/${estudio.id}`
     return this.http.delete<Estudios>(url)
   }
   editEstudio(estudios: Estudios): Observable<Estudios> {
-    const url = `${this.apiUrl}/${estudios.id}`
-    return this.http.put<Estudios>(url,estudios,httpOptions)
+    const url = `${this.apiUrlEst}/${estudios.id}`
+    return this.http.put<Estudios>(url, estudios, httpOptions)
   }
+  //      <<Fin de Educacion>>
+
+
+  //      <<Todo de Trabajos>>
+  obtenerDatosTrabajo(): Observable<any> {
+    return this.http.get(this.apiUrlTrab);
+  }
+  formTrabajo(trabajo:Trabajo): Observable<Trabajo> {
+    return this.http.post<Trabajo>(this.apiUrlTrab, trabajo, httpOptions);
+  }
+  editTrabajo(trabajo:Trabajo): Observable<Trabajo> {
+    const url = `${this.apiUrlTrab}/${trabajo.id}`
+    return this.http.put<Trabajo>(url, trabajo, httpOptions)
+  }
+  deleteTrabajo(trabajo: Trabajo): Observable<Trabajo> {
+    const url = `${this.apiUrlTrab}/${trabajo.id}`
+    return this.http.delete<Trabajo>(url)
+  }
+  //      <<Fin de Trabajos>>
+
 
 }
 

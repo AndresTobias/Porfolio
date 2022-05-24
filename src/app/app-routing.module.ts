1460e76ac019e5a829052a60bf87841fdff1b440
andres74ar@gmail.com <andres74ar@gmail.com>
2022-05-24 19:31:36 -0300

    puliendo un poco mas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { PorfolioComponent } from './porfolio/porfolio.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { RegisterComponent } from './register/register.component';
+import { TortasComponent } from './tortas/tortas.component';
 import { TrabajosComponent } from './trabajos/trabajos.component';
 const routes: Routes = [
   // { path: "", component: AppComponent },
@@ -14,6 +15,8 @@ const routes: Routes = [
   { path: 'principal', component: PrincipalComponent },
   { path: 'trabajos', component: TrabajosComponent },
   { path: 'acerca', component: AcercaDeComponent },
+  { path: 'tortas', component: TortasComponent },
+
   { path: '', redirectTo: 'login' , pathMatch: 'full' }
 ];
 

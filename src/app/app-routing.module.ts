f3796cfda2283ecaa73c9432a5e932a2fdce7467
andres74ar@gmail.com <andres74ar@gmail.com>
2022-05-31 17:01:42 -0300

    un login mas pituco

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { LoginDosComponent } from './login-dos/login-dos.component';
 import { LoginComponent } from './login/login.component';
 import { PorfolioComponent } from './porfolio/porfolio.component';
 import { PrincipalComponent } from './principal/principal.component';
@@ -16,7 +17,7 @@ const routes: Routes = [
   { path: 'trabajos', component: TrabajosComponent },
   { path: 'acerca', component: AcercaDeComponent },
   { path: 'tortas', component: TortasComponent },
-
+  { path: 'login-dos', component: LoginDosComponent },
   { path: '', redirectTo: 'login' , pathMatch: 'full' }
 ];
 
---
+++ src/app/login-dos/login-dos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-dos',
+  templateUrl: './login-dos.component.html',
+  styleUrls: ['./login-dos.component.css']
+})
+export class LoginDosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

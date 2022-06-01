048f8678677bb3f4426420529881edbd7b7b439c
andres74ar@gmail.com <andres74ar@gmail.com>
2022-06-01 08:25:51 -0300

    login con validadores

--- src/app/login-dos/login-dos.component.ts
+++ src/app/login-dos/login-dos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-login-dos',
@@ -7,10 +9,56 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login-dos.component.css']
 })
 export class LoginDosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form:FormGroup;
+  loginSi!:any;
+  constructor(private formBuilder:FormBuilder,
+              private userService:UsersService,
+              private router:Router 
+              ) 
+              
+  {
+    this.form=this.formBuilder.group({
+      nombreUsuario:['',[Validators.required,Validators.minLength(7)]],
+      password:['',[Validators.required,Validators.minLength(7)]]
+    })
   }
 
+ngOnInit(): void {
+  }
+get NombreUsuario(){
+  return this.form.get('nombreUsuario');
 }
+get Password(){
+  return this.form.get('password');
+}
+
+login() {
+  const user = { nombreUsuario: this.NombreUsuario?.value, password: this.Password?.value};
+  console.log(user);
+  this.userService.login(user).subscribe(
+    data => {
+      this.userService.setToken(data.token);
+      this.loginSi=this.userService.getToken();
+      //this.router.navigateByUrl('/');
+      console.log(this.loginSi);
+      if(this.loginSi)
+    {
+    console.log("entra en el if");
+    //window.location.reload();
+    this.router.navigateByUrl('/porfolio');
+    };
+      
+    },
+    error => {
+      console.log(error);
+    });
+    this.router.navigateByUrl('/');
+    this.irAlPrincipio();
+}
+irAlPrincipio(){
+if (this.loginSi===0){
+  this.router.navigateByUrl('/');
+} 
+}
+}
+
